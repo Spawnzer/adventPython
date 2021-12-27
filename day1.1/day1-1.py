79a814169778f05b279a3bb5d441cca9ef5c400e
spawnzer <alexandredubeau@hotmail.com>
2021-12-27 02:17:10 -0500

    python version#

---
+++ day1.1/day1-1.py
@@ -0,0 +1,11 @@
+lines = []
+with open("Day1Data.txt") as fd:
+    for line in fd:
+        lines.append(line.strip("\n"))
+i = 1
+c = 0
+while i < len(lines):
+    if int(lines[i]) > int(lines[i - 1]):
+            c = c + 1
+    i = i + 1
+print(c)
